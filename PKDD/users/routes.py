614b1bb2dfd14a38e52ad9a625e86ed01d5ac1cd
domestoscoockie <karol.grabowski852@gmail.com>
2025-03-01 17:51:21 +0100

    forms

--- PKDD/users/routes.py
+++ PKDD/users/routes.py
@@ -2,9 +2,18 @@ from flask import render_template, url_for, flash, redirect, request, Blueprint,
 send_from_directory
 from PKDD import db
 from flask import Blueprint
+from forms import RegistrationForm, LoginForm
 
 
 users = Blueprint('users', __name__)
+
+@users.route('/register')
+def register():
+
+
+    return render_template('register.html', title= 'PKDD-99 Statistics - Register')
+
+
 
 @users.route('/login')
 def login():
